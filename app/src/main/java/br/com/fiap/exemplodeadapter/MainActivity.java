e94617fa53730be0e6c0ae603db2bb5a7a8e3732
Fernando Mazola <fernando_mazola@hotmail.com>
2017-02-23 11:13:21 -0300

    Criado adaptor para o spinner e atribuindo valores ao spinner

--- app/src/main/java/br/com/fiap/exemplodeadapter/MainActivity.java
+++ app/src/main/java/br/com/fiap/exemplodeadapter/MainActivity.java
@@ -2,12 +2,23 @@ package br.com.fiap.exemplodeadapter;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.ArrayAdapter;
+import android.widget.Spinner;
 
 public class MainActivity extends AppCompatActivity {
+
+    Spinner spinner;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        //Atribuindo valor para ele, ATRELANDO XML com a ACTIVITY
+        spinner = (Spinner)findViewById(R.id.spinner);
+        //Criamos o adaptador e falamos que ele vai usar aquele layout de combobox e falando que dentro dele tem os cursos
+        String[] cursos = {"TIN", "TDS", "TRC", "SI", "ADM", "EC", "EP"};
+        ArrayAdapter<String>adapter = new ArrayAdapter<String>(this, android.R.layout.simple_spinner_dropdown_item, cursos);
+        spinner.setAdapter(adapter);
+
     }
 }
